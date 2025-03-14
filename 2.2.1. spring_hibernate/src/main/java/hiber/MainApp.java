cff0d817628c1656be0a0c9da70d54dc8c71b134
Vladimir Shemetenko <ov.shtk@gmail.com>
2025-03-14 10:06:16 +0300

    Расскомментировал Маин

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -21,10 +21,10 @@ public class MainApp {
         Car car3 = new Car("Fiat", 333);
         Car car4 = new Car("Honda", 4444);
 
-//        userService.add(new User("User1", "Lastname1", "user1@mail.ru", car1));
-//        userService.add(new User("User2", "Lastname2", "user2@mail.ru", car2));
-//        userService.add(new User("User3", "Lastname3", "user3@mail.ru", car3));
-//        userService.add(new User("User4", "Lastname4", "user4@mail.ru", car4));
+        userService.add(new User("User1", "Lastname1", "user1@mail.ru", car1));
+        userService.add(new User("User2", "Lastname2", "user2@mail.ru", car2));
+        userService.add(new User("User3", "Lastname3", "user3@mail.ru", car3));
+        userService.add(new User("User4", "Lastname4", "user4@mail.ru", car4));
 
         List<User> users = userService.listUsers();
         for (User user : users) {
