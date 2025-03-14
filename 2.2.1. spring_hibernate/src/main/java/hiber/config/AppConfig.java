6d872354ce7c208313b45d0e7f152d82326fed06
Vladimir Shemetenko <ov.shtk@gmail.com>
2025-03-14 09:46:31 +0300

    Заменил аннотацию @Autowired, на внедрение через конструктор (AppConfig, UserDaoImp, UserServiceImp). Добавил format_sql=true.Реализовал JOIN FETCH в запросе к БД.

--- 2.2.1. spring_hibernate/src/main/java/hiber/config/AppConfig.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/config/AppConfig.java
@@ -23,10 +23,13 @@ import java.util.Properties;
 @ComponentScan(value = "hiber")
 public class AppConfig {
 
-   @Autowired
-   private Environment env;
+   private final Environment env;
 
-   @Bean
+    public AppConfig(Environment env) {
+        this.env = env;
+    }
+
+    @Bean
    public DataSource getDataSource() {
       DriverManagerDataSource dataSource = new DriverManagerDataSource();
       dataSource.setDriverClassName(env.getProperty("db.driver"));
@@ -36,13 +39,15 @@ public class AppConfig {
       return dataSource;
    }
 
+
    @Bean
    public LocalSessionFactoryBean getSessionFactory() {
       LocalSessionFactoryBean factoryBean = new LocalSessionFactoryBean();
       factoryBean.setDataSource(getDataSource());
-      
+
       Properties props=new Properties();
       props.put("hibernate.show_sql", env.getProperty("hibernate.show_sql"));
+      props.put("hibernate.format_sql", env.getProperty("hibernate.format_sql"));
       props.put("hibernate.hbm2ddl.auto", env.getProperty("hibernate.hbm2ddl.auto"));
 
       factoryBean.setHibernateProperties(props);
--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
@@ -11,8 +11,11 @@ import java.util.List;
 @Repository
 public class UserDaoImp implements UserDao {
 
-    @Autowired
-    private SessionFactory sessionFactory;
+    private final SessionFactory sessionFactory;
+
+    public UserDaoImp(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
 
     @Override
     public void add(User user) {
@@ -22,14 +25,15 @@ public class UserDaoImp implements UserDao {
     @Override
     @SuppressWarnings("unchecked")
     public List<User> listUsers() {
-        TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery("from User");
+        TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery(
+                "SELECT u FROM User u JOIN FETCH u.car");
         return query.getResultList();
     }
 
     @Override
     public User getUserByCar(String model, int series) {
         TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery(
-                "SELECT u FROM User u WHERE u.car.model = :model AND u.car.series = :series"
+                "SELECT u FROM User u JOIN FETCH u.car WHERE u.car.model = :model AND u.car.series = :series"
         );
         query.setParameter("model", model);
         query.setParameter("series", series);
